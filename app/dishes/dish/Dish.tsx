c884cab99815d94c342898e89c99d81b3a033b7e
Asadov Elbotir <elbotir2008@gmail.com>
2023-11-11 16:45:45 +0500

    search bilan details

--- app/dishes/dish/Dish.tsx
+++ app/dishes/dish/Dish.tsx
@@ -1,13 +1,94 @@
+"use client";
 import "./dish.scss";
+import axios from "axios";
+import Link from "next/link";
+import { useState, useEffect } from "react";
+
 const Dish = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchedDishes, setSearchedDishes] = useState([]);
+  // const [currentPage, setCurrentPage] = useState(1);
+  // const [dishesPerPage] = useState(4);
+  // const totalPages = Math.ceil(searchedDishes.length / dishesPerPage);
+  // const pageNumbers = [];
+  // for (let i = 1; i <= totalPages; i++) {
+  //   pageNumbers.push(i);
+  // }
+
+  const searchDishes = async () => {
+    try {
+      let res = await axios.get(
+        `https://654ea70d358230d8f0ccbf59.mockapi.io/api/v1/Dishes?search=${searchQuery}&page=2&limit=8`
+      );
+      setSearchedDishes(res.data);
+      console.log(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    searchDishes();
+  }, [searchQuery]);
+
   return (
-    <section>
+    <section className="dishesPage-section">
       <div className="container">
-        {/* Input for Search */}
+        <input
+          type="text"
+          placeholder="Search dishes..."
+          className="searchBox"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
 
-        {/* Maping dishes */}
-
-        {/* Paginations */}
+        <div className="dishesPage-cards grid-class">
+          {searchedDishes.length > 0 ? (
+            searchedDishes.map((dsh) => (
+              <div className="card" key={dsh.id}>
+                <Link href={`/dishes/${dsh.id}`}>
+                  <img src={dsh.imgUrl} alt="Error" />
+                </Link>
+                <h4>{dsh.category}</h4>
+                <h2>{dsh.name}</h2>
+                <div className="flex-class">
+                  <span>24 min •</span>
+                  <img src="./Star.svg" alt="Error" />
+                  <span>{dsh.stars}</span>
+                </div>
+                <div className="flex-class">
+                  <p>{dsh.price}</p>
+                  <img src="./Add.svg" className="add" alt="Error" />
+                </div>
+              </div>
+            ))
+          ) : (
+            <p>No dishes found.</p>
+          )}
+        </div>
+        {/* <div className="pagination">
+          <button
+            onClick={() => setCurrentPage(currentPage - 1)}
+            disabled={currentPage === 1}
+          >
+            Previous
+          </button>
+          {pageNumbers.map((number) => (
+            <button
+              key={number}
+              onClick={() => setCurrentPage(number)}
+              className={currentPage === number ? "active" : ""}
+            >
+              {number}
+            </button>
+          ))}
+          <button
+            onClick={() => setCurrentPage(currentPage + 1)}
+            disabled={currentPage === totalPages}
+          >
+            Next
+          </button>
+        </div> */}
       </div>
     </section>
   );
--- app/homePage/dishes-section/Dishes.tsx
+++ app/homePage/dishes-section/Dishes.tsx
@@ -1,5 +1,27 @@
+"use client";
+import axios from "axios";
 import "./dishes.scss";
+import { useState, useEffect } from "react";
+
 const Dishes = () => {
+  const [dish, setDish] = useState([]);
+
+  const fetchDishes = async () => {
+    try {
+      let res = await axios.get(
+        "https://654ea70d358230d8f0ccbf59.mockapi.io/api/v1/Dishes?page=1&limit=4"
+      );
+      let data = res.data;
+      setDish(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchDishes();
+  }, []);
+
   return (
     <section className="dishes-section">
       <div className="container">
@@ -8,24 +30,27 @@ const Dishes = () => {
             Our Top <b>Dishes</b>
           </h1>
         </div>
-        <div className="dishes-cards">
-          <div className="card">
-            <img src="" alt="Eror" />
-            <img src="" alt="Eror" />
-            <h4></h4>
-            <h2></h2>
-            <div className="flex-class">
-              <span></span>
-              <img src="./Star.svg" alt="Eror" />
-              <span></span>
-            </div>
-            <div className="flex-class">
-              <p>
-                <b></b>
-              </p>
-              <h3></h3>
-            </div>
-          </div>
+        <div className="dishes-cards grid-class">
+          {dish.length > 0 ? (
+            dish.map((dsh, index) => (
+              <div className="card" key={index}>
+                <img src={dsh.imgUrl} alt="Eror" />
+                <h4>{dsh.category}</h4>
+                <h2>{dsh.name}</h2>
+                <div className="flex-class">
+                  <span>24 min •</span>
+                  <img src="./Star.svg" alt="Eror" />
+                  <span>{dsh.stars}</span>
+                </div>
+                <div className="flex-class">
+                  <p>{dsh.price}</p>
+                  <img src="./Add.svg" className="add" alt="Eror" />
+                </div>
+              </div>
+            ))
+          ) : (
+            <p>Loading dishes...</p>
+          )}
         </div>
       </div>
     </section>
